502322557966ef8ea621653ec60e3e6b00bfaddd
thisisyousif <thisisyousif@gmail.com>
2025-08-21 00:00:19 +0300

    Initial commit of my web site

---
+++ script.js
@@ -0,0 +1,13 @@
+const mobileButton=document.querySelector("#mobile-menu");
+const navItems=document.querySelector(".nav-items");
+const navLinks=document.querySelectorAll(".nav-link");
+console.log(navLinks);
+mobileButton.addEventListener('click',() => {
+    navItems.classList.toggle("active");
+})
+
+navLinks.forEach((navl)=>{
+    navl.addEventListener('click',()=>{
+        navItems.classList.toggle("active");
+    })
+})
